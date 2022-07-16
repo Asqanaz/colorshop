020580b94be99e3f53ad21776d304241dccc9188
Asqanaz <asmoyan.04@gmail.com>
2022-07-16 19:42:02 +0400

    latest commit

--- src/common/items/Items.jsx
+++ src/common/items/Items.jsx
@@ -1,39 +1,48 @@
-import React, { useEffect, useState } from "react";
-import "./items.css";
-import CartItems from "./CartItems";
-import cartItems from "../../data/cartItems.json";
+import React, { useEffect, useState } from "react"
+import "./items.css"
+import CartItems from "./CartItems"
+import cartItems from "../../data/cartItems.json"
+import { inputAdornmentClasses } from "@mui/material"
 
 const Items = () => {
-  const [availableCartItems, setAvailableCartItems] = useState([]);
-  const [cartItemsState, setCartItemsState] = useState(cartItems);
+	const [cartItemsState, setCartItemsState] = useState(cartItems)
+	const [pickedItems, setPickedItems] = useState([])
+	const [itemsQuantity, setItemQuantity] = useState(pickedItems.length)
+	const handleAddCart = (e) => {
+		setCartItemsState(
+			cartItemsState.map((item) =>
+				item.id === e.id
+					? { ...item, isAvailable: !item.isAvailable }
+					: { ...item }
+			)
+		)
 
-  const handleAddCart = (e) => {
-    setCartItemsState(
-      cartItemsState.map((item) =>
-        item.id === e.id
-          ? { ...item, isAvailable: !item.isAvailable }
-          : { ...item }
-      )
-    );    
-  };  
+	}
   
-  
-  console.log(availableCartItems);
-  console.log(cartItemsState);
-  return (
-    <div className="items-container">
-      {cartItems.map((item) => (
-        <CartItems
-          key={item.id}
-          id={item.id}
-          hexcode={item.hexcode}
-          name={item.name}
-          price={item.price}
-          onClick={handleAddCart.bind(this, item)}
-        />
-      ))}
-    </div>
-  );
-};
+	useEffect(() => {
+    cartItemsState.forEach((item) => {
+      item.isAvailable
+        ? setPickedItems([...pickedItems, item])
+        : { ...pickedItems }
+    })
+    }, [cartItemsState])
 
-export default Items;
+	console.log(cartItemsState)
+	console.log(pickedItems)
+	return (
+		<div className="items-container">
+			{cartItems.map((item) => (
+				<CartItems
+					key={item.id}
+					id={item.id}
+					hexcode={item.hexcode}
+					name={item.name}
+					price={item.price}
+					onClick={handleAddCart.bind(this, item)}
+				/>
+			))}
+		</div>
+	)
+}
+
+export default Items
